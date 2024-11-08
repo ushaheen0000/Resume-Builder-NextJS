c442036a0a0ff6fa27fc7a5317fb6ced058d825a
Uzma Shaheen <ushaheen0000@gmail.com>
2024-11-07 17:22:00 -0800

    created resume builder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,172 @@
+"use client";
 import Image from "next/image";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { jsPDF } from "jspdf";
+import { url } from "inspector";
+interface resumeFormData {
+  image: FileList | null;
+  fullName: string;
+  email: string;
+  phoneNumber: number;
+  education: string;
+  skills: string;
+  experience: string;
+}
 
-export default function Home() {
+const ResumeBuilder: React.FC = () => {
+  const { register, handleSubmit } = useForm<resumeFormData>();
+  const [resumeData, setResumeData] = useState<resumeFormData | null>(null);
+  const [imagePreview, setImagePreview] = useState<string | null>(null)
+
+  const onSubmit: SubmitHandler<resumeFormData> = (data) => {
+    setResumeData(data);
+  }
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files ? e.target.files[0] : null;
+
+
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+
+      setImagePreview(imageUrl);
+    }
+  }
+  const handleDownloadPDF = () => {
+    if (!resumeData) return
+    const doc = new jsPDF();
+    if (imagePreview) {
+      doc.addImage(imagePreview, 'JPEG', 20, 20, 40, 40);
+    }
+
+    doc.text(`${resumeData.fullName}`, 20, 80);
+    doc.text(`${resumeData.email}`, 20, 90);
+    doc.text(`${resumeData.phoneNumber}`, 20, 100);
+    doc.text(`${resumeData.education}`, 20, 110);
+    doc.text(`${resumeData.skills}`, 20, 120);
+    doc.text(`${resumeData.experience}`, 20, 130);
+    doc.save('resume.pdf')
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="max-w-screen-lg mx-auto">
+
+      <div className="relative w-full h-[250vh] bg-fixed bg-cover bg-center pt-5"
+        style={{ backgroundImage: 'url(/tea.jpeg)' }}
+      >
+
+        <div className="relative max-w-4xl mx-auto shadow-lg rounded-lg bg-[rgb(0,0,0,0.5)] p-10 items-center py-4">
+          <h1 className="text-4xl text-[#b7dd2e] font-bold mt-10 text-center">Resume Builder</h1>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 ">
+            <div>
+              <label className="block text-[#cefa31]">Full Name</label>
+              <input type="text"
+                {...register('fullName')}
+                placeholder="Enter Your name"
+                required
+                className="mt-1 p-2 w-full rounded-md focus:outline-none bg-transparent border-[1px] text-white"
+              />
+            </div>
+            <div>
+              <label className="block text-[#cefa31]">Email Address</label>
+              <input type="text"
+                {...register('email')}
+                placeholder="Enter Your Email Address"
+                required
+                className="mt-1 p-2 w-full rounded-md focus:outline-none bg-transparent border-[1px] text-white"
+              />
+            </div>
+            <div>
+              <label className="block text-[#cefa31]">Mobile Number</label>
+              <input type="text"
+                {...register('phoneNumber')}
+                placeholder="Enter Your phone number"
+                required
+                className="mt-1 p-2 w-full rounded-md focus:outline-none bg-transparent border-[1px] text-white"
+              />
+            </div>
+            <div>
+              <label className="block text-[#cefa31]">Education
+              </label>
+              <textarea
+                {...register('education')}
+                placeholder="Your education"
+                required
+                className="mt-1 p-2 w-full rounded-md focus:outline-none bg-transparent border-[1px] text-white "
+              />
+            </div>
+            <div>
+              <label className="block text-[#cefa31]">Skills</label>
+              <textarea
+                {...register('skills')}
+                placeholder="Your Skills"
+                required
+                className="mt-1 p-2 w-full rounded-md focus:outline-none bg-transparent border-[1px] text-white "
+              />
+            </div>
+
+            <div>
+              <label className="block text-[#cefa31]">Experience</label>
+              <textarea
+                {...register('experience')}
+                placeholder="Your experience"
+                required
+                className="mt-1 p-2 w-full rounded-md focus:outline-none bg-transparent border-[1px] text-white "
+              />
+            </div>
+            <div>
+              <label className="block text-[#cefa31]">Profile Image</label>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                width={100}
+                height={100}
+                required
+                className="mt-1 p-2 w-full rounded-md focus:outline-none bg-transparent border-[1px] text-white object-cover"
+              />
+            </div>
+            <button type="submit" className="hover:border bg-[#a8cc25] rounded-[0.6rem] px-3 py-2 hover:bg-transparent  text-black hover:text-white">Submit Now</button>
+          </form>
+        </div>
+        <div className="relative max-w-4xl mx-auto shadow-lg rounded-lg bg-[rgb(0,0,0,0.5)] p-10 items-center mt-5">
+          {resumeData && (
+            <div className="mt-8">
+              <h2 className="text-2xl font-semibold mb-4 text-white text-center">Your Resume</h2>
+              {imagePreview && (
+                <div className="mb-4 flex justify-center items-center">
+                  <Image
+                    src={imagePreview}
+                    alt="Profile Preview"
+                    width={100} height={100}
+
+                    className=" rounded-full object-cover border-2 border-[#cefa31] "
+                  />
+                </div>
+              )}
+              <div className="items-center text-[#cefa31] text-center">
+                <h3 className="text-xl font-bold text-white">{resumeData.fullName}</h3>
+                <p> {resumeData.email}</p>
+                <p> {resumeData.phoneNumber}</p>
+                <p> {resumeData.education}</p>
+                <p> {resumeData.skills}</p>
+                <p> {resumeData.experience}</p>
+              </div>
+              <div className="flex justify-center items-center">
+                <button
+                  onClick={handleDownloadPDF}
+                  className="mt-4 bg-[#a9ce23] text-black hover:text-white py-2 rounded-md hover:bg-transparent hover:border-[1px] px-4 "
+                >
+                  Download as PDF
+                </button>
+
+              </div>
+
+            </div>
+          )}
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
+export default ResumeBuilder;
